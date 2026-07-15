84414ea82839ffddc5247d365e41758215e23d06
1IMperaDOR0 <lucaslucore@gmail.com>
2026-07-15 20:24:46 -0300

    Fixed base in vite.config.ts to the option of Vercel

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: process.env.NODE_ENV === 'production'
+    ? (process.env.VERCEL ? '/' : '/portfolio/')
+    : '/',
   plugins: [react(), tailwindcss()],
 })
